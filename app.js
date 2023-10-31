afc8145ebbb759174ca42d646bceee57bc23089e
Breakerofcode73 <egoreremin3@gmail.com>
2023-10-31 11:10:52 +0300

    first load

---
+++ app.js
@@ -0,0 +1,12 @@
+const users=[
+    {userName:'David',
+    status:'online',
+    lastActivity:10},
+    {userName:'Lucy',
+    status:'offline',
+    lastActivity:22},
+    {userName:'Bob',
+    status:'online',
+    lastActivity:104}]
+usersOnlinename=[]
+for(const element of users){console.log(e)}
